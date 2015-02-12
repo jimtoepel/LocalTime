da5061f2a5ed78043a2e3537727ea773a23ac3c7
jimtoepel <jim.toepel@gmail.com>
2015-02-11 20:14:55 -0800

    Base up and running
    Next, read out current month/day/etc.
    
    Then add 1million seconds and do it again.

--- LocalTime/main.c
+++ LocalTime/main.c
@@ -7,9 +7,17 @@
 //
 
 #include <stdio.h>
+#include <time.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    float secondsSince1970 = time(NULL);
+    printf("It has been %.0f seconds since 1970\n", secondsSince1970);
+    
+    struct tm now;
+    localtime_r(&secondsSince1970, &now);
+    printf("The time in Greenwich is %d:%d:%d\n", now.tm_hour, now.tm_min, now.tm_sec);
+    
+    
     return 0;
 }
