8b1af16bbe6a2a7904162d780116c10bbce79eb6
jimtoepel <jim.toepel@gmail.com>
2015-02-11 20:05:12 -0800

    Initial Commit

---
+++ LocalTime/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  LocalTime
+//
+//  Created by Jim Toepel on 2/11/15.
+//  Copyright (c) 2015 FunderDevelopment. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
